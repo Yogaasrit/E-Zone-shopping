e9655f112e67f7e3243e1273272a0603a711379b
Yogaasrit <83397145+Yogaasrit@users.noreply.github.com>
2023-07-12 17:27:52 +0530

    Updated Header with CSS

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,30 @@
+
 import React from 'react'
+import MenuList from '../MenuList/MenuList'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <nav className="navbar navbar-expand-md fixed-top" style={{backgroundColor: "#F07A91",color:"#e3f2fd"}}>
+          <div className="container-fluid">
+            <a className="navbar-brand" 
+               href="/">E-Zone-Shopping-Mart 
+           </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon" />
+            </button>
+            <MenuList />
+            <div className="collapse navbar-collapse" id="navbarCollapse"></div>
+          </div>
+      </nav>
+      </header>
   )
 }
 
