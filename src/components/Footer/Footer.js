2676da5e64700b56da1cf638394a336ebdc73ba7
Yogaasrit <83397145+Yogaasrit@users.noreply.github.com>
2023-07-12 17:28:14 +0530

    Created Footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const Footer = () => {
+  return (
+    <div>
+      <h5 className = "text-center">
+        COPYRIGHTS | 2023 
+      </h5>
+      
+    </div>
+  )
+}
+
+export default Footer
