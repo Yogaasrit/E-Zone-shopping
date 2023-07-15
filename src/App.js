c8531fb623a52a1653f41fa1cef0a16b6d7a7e45
Yogaasrit <83397145+Yogaasrit@users.noreply.github.com>
2023-07-15 21:12:28 +0530

    Made as Single Page Application

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import './App.css';
-import HomePage from './Pages/HomePage/HomePage';
 import 'bootstrap/dist/css/bootstrap.css';
-
+import Header from './components/Header/Header';
+import HomePage from './Pages/HomePage/HomePage';
+import AboutUs from './Pages/AboutUs/AboutUs';
+import ContactUs from './Pages/ContactUs/ContactUs';
+import Footer from './components/Footer/Footer';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 
 
 function App() {
   return (
+    <BrowserRouter>
     <div>    
-      <HomePage/>
+      <Header/>
+      <main className = "container mt-5 pt-3"> 
+
+      <Routes>
+        <Route path = '/' element = {<HomePage/>} ></Route>
+        <Route path = '/about-us' element = {<AboutUs/>} ></Route>
+        <Route path = '/contact-us' element = {<ContactUs/>} ></Route>
+      </Routes>
+      </main>
+      <Footer/>
     </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import MenuList from '../MenuList/MenuList'
 
 
 const Footer = () => {
   return (
     <div>
       <h5 className = "text-center">
+      <hr></hr>
         COPYRIGHTS | 2023 
+        <hr></hr>
+        <MenuList/>
       </h5>
       
     </div>
