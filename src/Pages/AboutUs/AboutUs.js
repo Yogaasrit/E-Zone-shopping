1691f69f470865ff66b1e90124859bff65dc43ed
Yogaasrit <83397145+Yogaasrit@users.noreply.github.com>
2023-07-15 21:13:44 +0530

    Updated Helmet

---
+++ src/Pages/AboutUs/AboutUs.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+const AboutUs = () => {
+  return (
+    <div>
+        <Helmet>
+                <title>About-us</title>
+        </Helmet>
+        <h4>About us</h4>
+    </div>
+  )
+}
+
+export default AboutUs
---
+++ src/Pages/ContactUs/ContactUs.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+const ContactUs = () => {
+  return (
+    <div>
+        <Helmet>
+                <title>Contact-us</title>
+        </Helmet>
+        <h4>ContactUs</h4>
+    </div>
+  )
+}
+
+export default ContactUs
