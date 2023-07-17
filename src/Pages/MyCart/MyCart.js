dda269ef929b5b5326882da8206e4652e90d0154
Yogaasrit <83397145+Yogaasrit@users.noreply.github.com>
2023-07-17 22:24:28 +0530

    Added Cart

---
+++ src/Pages/MyCart/MyCart.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+
+const MyCart = () => {
+  return (
+    <>
+    <Helmet>
+        <title>Cart</title>
+    </Helmet>
+     <div>MyCart</div>
+     </>
+  )
+}
+
+export default MyCart
