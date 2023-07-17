74ba096b04a661646662c4e2d1ed0bb06014602b
Yogaasrit <83397145+Yogaasrit@users.noreply.github.com>
2023-07-17 22:24:51 +0530

    Created offer component

---
+++ src/Pages/Offers/Offers.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import { Helmet } from 'react-helmet'
+
+export class Offers extends Component {
+  render() {
+    return (
+        <div>
+            <Helmet>
+                <title>Offers</title>
+            </Helmet>
+            <div>Offers</div>
+
+        </div>
+      
+    )
+  }
+}
+
+export default Offers
